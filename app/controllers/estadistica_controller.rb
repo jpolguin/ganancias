990d5b2dc177ba9a84ad785f12b7302a4e4c23f5
Juan Pablo Olguin <jpolguin@gmail.com>
2010-09-29 23:51:36 -0300

    agregado estadisticas de agosto, septiembre 2010. Intento con tabs de jquery, no funciono todavia. comiteo igual se ven como links en la misma pagina.

--- app/controllers/estadistica_controller.rb
+++ app/controllers/estadistica_controller.rb
@@ -5,21 +5,36 @@ class EstadisticaController < ApplicationController
   
   
   def estadistica_por_rubro
-    @estadisticas_rubro = []
-    @estadisticas_rubro = EstadisticaPorRubroMes.find_estadisticas_mes("julio", "2010")
+    @estadisticas_rubro_julio = []
+    @estadisticas_rubro_julio = EstadisticaPorRubroMes.find_estadisticas_mes("julio", "2010")
+
+    @estadisticas_rubro_agosto = []
+    @estadisticas_rubro_agosto = EstadisticaPorRubroMes.find_estadisticas_mes("agosto", "2010")
+
+    @estadisticas_rubro_septiembre = []
+    @estadisticas_rubro_septiembre = EstadisticaPorRubroMes.find_estadisticas_mes("septiembre", "2010")
+
     
   end
   
    def estadistica_por_cargo
-    @estadisticas_cargo = []
-    @estadisticas_cargo = EstadisticaPorCargoMes.find_estadisticas_mes("julio", "2010")
+    @estadisticas_cargo_julio = []
+    @estadisticas_cargo_julio = EstadisticaPorCargoMes.find_estadisticas_mes("julio", "2010")
+
+    @estadisticas_cargo_agosto = []
+    @estadisticas_cargo_agosto = EstadisticaPorCargoMes.find_estadisticas_mes("agosto", "2010")
+     
+    @estadisticas_cargo_septiembre = []
+    @estadisticas_cargo_septiembre = EstadisticaPorCargoMes.find_estadisticas_mes("septiembre", "2010")
     
+     
+     
   end
   
   def estadistica_promedio_anual
      @estadisticas_promedio = []
      #anio 2010
-     @estadisticas_promedio = [{:mes => "enero",:salario_promedio=>6990.30,:anio=>"2010"},{:mes => "febrero",:salario_promedio=>7030.30,:anio=>"2010"},{:mes => "marzo",:salario_promedio=>7325.67,:anio=>"2010"},{:mes => "abril",:salario_promedio=>7289.43,:anio=>"2010"},{:mes => "mayo",:salario_promedio=>7794.96,:anio=>"2010"},{:mes => "junio",:salario_promedio=>7989.30,:anio=>"2010"},{:mes => "julio",:salario_promedio=>7895.71,:anio=>"2010"},{:mes => "agosto",:salario_promedio=>8663,:anio=>"2010"} ]
+     @estadisticas_promedio = [{:mes => "enero",:salario_promedio=>6990.30,:anio=>"2010"},{:mes => "febrero",:salario_promedio=>7030.30,:anio=>"2010"},{:mes => "marzo",:salario_promedio=>7325.67,:anio=>"2010"},{:mes => "abril",:salario_promedio=>7289.43,:anio=>"2010"},{:mes => "mayo",:salario_promedio=>7794.96,:anio=>"2010"},{:mes => "junio",:salario_promedio=>7989.30,:anio=>"2010"},{:mes => "julio",:salario_promedio=>7895.71,:anio=>"2010"},{:mes => "agosto",:salario_promedio=>8739.45,:anio=>"2010"},{:mes => "septiembre",:salario_promedio=>8935.47,:anio=>"2010"} ]
   end
    
 end
--- db/migrate/034_add_estadistica_julio2010.rb
+++ db/migrate/034_add_estadistica_julio2010.rb
@@ -2,21 +2,21 @@ class AddEstadisticaJulio2010 < ActiveRecord::Migration
   def self.up
     execute "INSERT INTO estadistica_por_rubro_mes  (rubro_empresa, salario_promedio, mes,anio)  " +
      " SELECT rubro_empresa as rubro_empresa, avg(gross_salary) as  salario_promedio, 'julio' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
-     " where ms.salary_id = s.id and ms.month like '%mayo%' and rubro_empresa <> 'ninguno' and " +
-    " gross_salary > 3000 and gross_salary < 59000 and rubro_empresa <> 'IBM'  " +
-    "  group by rubro_empresa    having count(*) > 25 "
+     " where ms.salary_id = s.id and ms.month like '%mayo%' and rubro_empresa <> 'ninguno' and rubro_empresa <> 'ninguna' and rubro_empresa <> 'niguno'   and " +
+    " gross_salary > 3000 and gross_salary < 60000 and rubro_empresa <> 'IBM'  " +
+    "  group by rubro_empresa    having count(*) > 15 "
 
     execute "INSERT INTO estadistica_por_cargo_mes  (cargo, salario_promedio, mes,anio)  " +
      "  SELECT  cargo as cargo, avg(gross_salary) as  salario_promedio, 'julio' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
-    " where ms.salary_id = s.id and ms.month like '%mayo%' and cargo <> 'ninguno' and " + 
-    " gross_salary > 3000 and gross_salary < 59000 and cargo <> 'IBM' " + 
-    " group by cargo having count(*) > 10 "
-
+    " where ms.salary_id = s.id and ms.month like '%mayo%' and cargo <> 'ninguno' and cargo <> 'ninguna' and cargo <> 'niguno'  and " + 
+    " gross_salary > 3000 and gross_salary < 60000 and cargo <> 'IBM' " + 
+    " group by cargo having count(*) > 5 "
     
   end
 
   def self.down
      EstadisticaPorRubroMes.delete_all   "mes = 'julio' and anio = '2010'"
      EstadisticaPorCargoMes.delete_all  "mes = 'julio' and anio = '2010'"     
+
   end
 end
