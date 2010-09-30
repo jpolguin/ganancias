98bcd72558d26036c84fbe5bee9f01f5554da7a4
Juan Pablo Olguin <jpolguin@gmail.com>
2010-09-30 00:26:24 -0300

    cambio a estadisticas_agosto_septiembre

--- db/migrate/035_estadisticas_agosto_septiembre2010.rb
+++ db/migrate/035_estadisticas_agosto_septiembre2010.rb
@@ -3,31 +3,31 @@ class EstadisticasAgostoSeptiembre2010 < ActiveRecord::Migration
      execute "INSERT INTO estadistica_por_rubro_mes  (rubro_empresa, salario_promedio, mes,anio)  " +
      " SELECT rubro_empresa as rubro_empresa, avg(gross_salary) as  salario_promedio, 'agosto' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
      " where ms.salary_id = s.id and ms.month like '%mayo%' and rubro_empresa <> 'ninguno' and rubro_empresa <> 'ninguna' and rubro_empresa <> 'niguno'   and " +
-    " gross_salary > 3000 and gross_salary < 60000 and rubro_empresa <> 'IBM' and month(fecha_calculo) = 8  " +
+    " gross_salary > 3000 and gross_salary < 60000 and rubro_empresa <> 'IBM' and (extract(month from timestamp fecha_calculo)+1) = 8  " +
     "  group by rubro_empresa    having count(*) > 10 "
 
     execute "INSERT INTO estadistica_por_cargo_mes  (cargo, salario_promedio, mes,anio)  " +
      "  SELECT  cargo as cargo, avg(gross_salary) as  salario_promedio, 'agosto' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
     " where ms.salary_id = s.id and ms.month like '%mayo%' and cargo <> 'ninguno' and cargo <> 'ninguna' and cargo <> 'niguno'  and " + 
-    " gross_salary > 3000 and gross_salary < 60000 and cargo <> 'IBM' and month(fecha_calculo) = 8 " + 
+    " gross_salary > 3000 and gross_salary < 60000 and cargo <> 'IBM' and (extract(month from timestamp fecha_calculo)+1) = 8 " + 
     " group by cargo having count(*) > 3 "
 
 
     execute "INSERT INTO estadistica_por_rubro_mes  (rubro_empresa, salario_promedio, mes,anio)  " +
      " SELECT rubro_empresa as rubro_empresa, avg(gross_salary) as  salario_promedio, 'septiembre' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
      " where ms.salary_id = s.id and ms.month like '%mayo%' and rubro_empresa <> 'ninguno' and rubro_empresa <> 'ninguna' and rubro_empresa <> 'niguno'   and " +
-    " gross_salary > 3000 and gross_salary < 60000 and rubro_empresa <> 'IBM' and month(fecha_calculo) = 9  " +
+    " gross_salary > 3000 and gross_salary < 60000 and rubro_empresa <> 'IBM' and (extract(month from timestamp fecha_calculo)+1) = 9  " +
     "  group by rubro_empresa   having count(*) > 10 "
 
     execute "INSERT INTO estadistica_por_cargo_mes  (cargo, salario_promedio, mes,anio)  " +
      "  SELECT  cargo as cargo, avg(gross_salary) as  salario_promedio, 'septiembre' as mes , '2010' as anio FROM salaries s, monthly_salaries ms " + 
     " where ms.salary_id = s.id and ms.month like '%mayo%' and cargo <> 'ninguno' and cargo <> 'ninguna' and cargo <> 'niguno'  and " + 
-    " gross_salary > 3000 and gross_salary < 60000 and cargo <> 'IBM' and month(fecha_calculo) = 9 " + 
+    " gross_salary > 3000 and gross_salary < 60000 and cargo <> 'IBM' and (extract(month from timestamp fecha_calculo)+1) = 9 " + 
     " group by cargo having count(*) > 3 "
 
-
-    
-  end
+                  
+      
+  end                                                                                   
 
   def self.down
      EstadisticaPorRubroMes.delete_all   "mes = 'agosto' and anio = '2010'"
